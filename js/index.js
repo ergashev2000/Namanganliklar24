e8ca8b7c75d955f292407ecb5d1c3d3d7a3964f8
Islomjon Ergashev <76722837+ergashevislomjon@users.noreply.github.com>
2022-07-06 12:50:12 +0500

    New

---
+++ js/index.js
@@ -0,0 +1,13 @@
+function div() {
+    var element = document.getElementById("test");
+    element.classList.remove("alert_bg");
+
+    var close = document.getElementById("non");
+    setTimeout(function(){ close.style.display = "none";}, 300);
+}
+
+
+
+
+    
+    
